b44c1f06d79af0770d74e304a505d62700e78365
Oleksii Verbytskyi <alverba88@gmail.com>
2023-08-07 12:38:46 +0300

    added overview component

---
+++ src/data/overview.js
@@ -0,0 +1,16 @@
+const context = {
+  items: [
+    {
+      title: "Plan your perfect trip",
+      content: "Li Europan lingues es membres del sam familie separat existentie es un myth."
+    },
+    {
+      title: "Get your tickets",
+      content: "Li Europan lingues es membres del sam familie separat existentie es un myth"
+    },
+    {
+      title: "Travel around country",
+      content: "Li Europan lingues es membres del sam familie separat existentie es un myth"
+    }
+  ]
+};
---
+++ src/service/app.js
@@ -0,0 +1,4 @@
+// import overviewListTpl from "../template/overview.hbs";
+// import overview_DATA from "../data/overview";
+//
+// console.log(overview_DATA)
