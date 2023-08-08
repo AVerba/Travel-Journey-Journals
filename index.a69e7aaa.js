4228c781c46f901eb4f3e8e997fd41cd0f8083c9
AVerba <57949955+AVerba@users.noreply.github.com>
2023-08-08 17:57:39 +0000

    Deploying to gh-pages from @ AVerba/Travel-Journey-Journals@ea95d1dd5dd8a97ccae7b10a8c5ddfb397b5d150 🚀

---
+++ index.a69e7aaa.js
@@ -0,0 +1,2 @@
+new Swiper(".packages__swiper",{slidesPerView:3,spaceBetween:84,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}}),new Swiper(".destinations__swiper",{slidesPerView:3,spaceBetween:84,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.a69e7aaa.js.map
---
+++ index.af8a0ff2.js
@@ -0,0 +1,2 @@
+new Swiper(".packages__swiper",{slidesPerView:3,spaceBetween:84,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}}),new Swiper(".destinations__swiper",{slidesPerView:3,spaceBetween:84,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.af8a0ff2.js.map
