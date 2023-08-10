f6cc8d04a94527987614e98dc99dd928f5d28030
AVerba <57949955+AVerba@users.noreply.github.com>
2023-08-10 14:14:40 +0000

    Deploying to gh-pages from @ AVerba/Travel-Journey-Journals@3e8700e436c050b130e223f88118d5e7819c651e 🚀

--- index.a69e7aaa.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".packages__swiper",{slidesPerView:3,spaceBetween:84,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}}),new Swiper(".destinations__swiper",{slidesPerView:3,spaceBetween:84,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.a69e7aaa.js.map
