ce4874961c61a81aa42e9b851eec148b6b4342e8
Oleksii Verbytskyi <alverba88@gmail.com>
2023-08-16 18:13:04 +0300

    mod dest data file for hbs

--- src/data/destinations.js
+++ src/data/destinations.js
@@ -1,6 +1,6 @@
 const destinations = [
   {
-    imgSrc: 'https://github.com/AVerba/Travel-Journey-Journals/blob/gh-pages/city.e417256f.jpg?raw=true',
+    imgSrc: 'https://github.com/AVerba/Travel-Journey-Journals/blob/main/assets/destinations/city.jpg?raw=true',
     imgAlt: 'Calle Gran Via',
     location: 'Madrid, Spain',
     itemName: 'Calle Gran Via',
@@ -9,7 +9,7 @@ const destinations = [
     description: 'Li Europan lingues es membres del sam lor separat existentie es un myth.',
   },
   {
-    imgSrc: 'paris.jpg',
+    imgSrc: 'https://github.com/AVerba/Travel-Journey-Journals/blob/main/assets/destinations/paris.jpg?raw=true',
     imgAlt: 'Eiffel Tower',
     location: 'Champ de Mars, France',
     itemName: 'Eiffel Tower',
@@ -18,12 +18,39 @@ const destinations = [
     description: 'Li Europan lingues es membres del sam lor separat existentie es un myth.',
   },
   {
-    imgSrc: 'beach.jpg',
+    imgSrc: 'https://github.com/AVerba/Travel-Journey-Journals/blob/main/assets/destinations/beach.jpg?raw=true',
     imgAlt: 'Diamond Beach',
     location: 'Bali, Indonesia',
     itemName: 'Diamond Beach',
     rating: '5 stars',
     price: '$499',
+    description: 'Li Europan lingues es membres del sam lor separat existentie es un myth.',
+  },
+  {
+    imgSrc: 'https://github.com/AVerba/Travel-Journey-Journals/blob/main/assets/destinations/city.jpg?raw=true',
+    imgAlt: 'Calle Gran Via',
+    location: 'Madrid, Spain',
+    itemName: 'Calle Gran Via',
+    rating: '5 stars',
+    price: '$129',
+    description: 'Li Europan lingues es membres del sam lor separat existentie es un myth.',
+  },
+  {
+    imgSrc: 'https://github.com/AVerba/Travel-Journey-Journals/blob/main/assets/destinations/paris.jpg?raw=true',
+    imgAlt: 'Eiffel Tower',
+    location: 'Champ de Mars, France',
+    itemName: 'Eiffel Tower',
+    rating: '5 stars',
+    price: '$799',
+    description: 'Li Europan lingues es membres del sam lor separat existentie es un myth.',
+  },
+  {
+    imgSrc: 'https://github.com/AVerba/Travel-Journey-Journals/blob/main/assets/destinations/beach.jpg?raw=true',
+    imgAlt: 'Diamond Beach',
+    location: 'Bali, Indonesia',
+    itemName: 'Diamond Beach',
+    rating: '5 stars',
+    price: '$1299',
     description: 'Li Europan lingues es membres del sam lor separat existentie es un myth.',
   },
 ];
