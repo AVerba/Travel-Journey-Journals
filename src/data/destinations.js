ac30e0c4b70aa8789c2124d1bb25741448c08996
Oleksii Verbytskyi <alverba88@gmail.com>
2023-08-16 18:10:52 +0300

    mod dest hbs

--- src/data/destinations.js
+++ src/data/destinations.js
@@ -1,12 +1,31 @@
-const destinationsData = [
+const destinations = [
   {
-    imgSrc: './images/destinations/city.jpg',
+    imgSrc: 'https://github.com/AVerba/Travel-Journey-Journals/blob/gh-pages/city.e417256f.jpg?raw=true',
     imgAlt: 'Calle Gran Via',
     location: 'Madrid, Spain',
-    title: 'Calle Gran Via',
-    rate: '5 stars',
-    price: '399',
+    itemName: 'Calle Gran Via',
+    rating: '5 stars',
+    price: '$399',
     description: 'Li Europan lingues es membres del sam lor separat existentie es un myth.',
   },
-]
+  {
+    imgSrc: 'paris.jpg',
+    imgAlt: 'Eiffel Tower',
+    location: 'Champ de Mars, France',
+    itemName: 'Eiffel Tower',
+    rating: '5 stars',
+    price: '$599',
+    description: 'Li Europan lingues es membres del sam lor separat existentie es un myth.',
+  },
+  {
+    imgSrc: 'beach.jpg',
+    imgAlt: 'Diamond Beach',
+    location: 'Bali, Indonesia',
+    itemName: 'Diamond Beach',
+    rating: '5 stars',
+    price: '$499',
+    description: 'Li Europan lingues es membres del sam lor separat existentie es un myth.',
+  },
+];
 
+export default destinations;
--- src/service/app.js
+++ src/service/app.js
@@ -1,2 +1,21 @@
 import Handlebars from 'handlebars';
-import destinationsTemplate from '../template/destinations.hbs';
+
+import destinations from '../data/destinations.js';
+import destinationTemplate from '../template/destinations.hbs';
+
+console.log(destinations);
+Handlebars.registerHelper('imgPath', function(imgSrc) {
+  return `./images/destinations/${imgSrc}`;
+});
+const imageBaseUrl = '';
+const destinationDataWithImagePaths = destinations.map(destination => {
+  return {
+    ...destination,
+    imgSrc: imageBaseUrl + destination.imgSrc
+  };
+});
+
+const destinationContainer = document.querySelector('.destinations__swiper');
+ const renderer = destinationTemplate({ destinations: destinationDataWithImagePaths });
+ console.log(renderer)
+ destinationContainer.insertAdjacentHTML('beforeend', renderer);
